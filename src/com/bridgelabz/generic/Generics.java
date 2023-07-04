31dede48ab134f4bea53f91e0172e13457e17188
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-07-04 10:30:29 +0530

    Find Maximum Problem using Generics

---
+++ src/com/bridgelabz/generic/Generics.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.generic;
+
+public class Generics {
+
+    public static void main(String[] args) {
+        System.out.println("Find Maximum Problem using Generics");
+       
+    }
+}
