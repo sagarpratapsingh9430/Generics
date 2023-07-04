8d3bef87aa1450e35a5602ecdb64cc5e57246e45
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-07-04 10:54:31 +0530

    Given 3 Floats find the maximum

--- src/com/bridgelabz/generic/Generics.java
+++ src/com/bridgelabz/generic/Generics.java
@@ -1,9 +1,34 @@
 package com.bridgelabz.generic;
 
 public class Generics {
-
+    void maxNumberInInteger(Integer x, Integer y, Integer z){
+        int max = x;
+        if (x.compareTo(y)>0 && x.compareTo(z)>0){
+            max = x;
+        } else if (y.compareTo(z)>0) {
+            max = y;
+        }else {
+            max = z;
+        }
+        System.out.println("Maximum number from " + x +", " +y +" and "+ z + " is: " + max);
+    }
+    void maxNumberInFloat(Float x, Float y, Float z){
+        float max = x;
+        if (x.compareTo(y)>0 && x.compareTo(z)>0){
+            max = x;
+        } else if (y.compareTo(z)>0) {
+            max = y;
+        }else {
+            max = z;
+        }
+        System.out.println("Maximum number from " + x +", " +y +" and "+ z + " is: " + max);
+    }
     public static void main(String[] args) {
         System.out.println("Find Maximum Problem using Generics");
+       Generics generics = new Generics();
        
+       generics.maxNumberInInteger(5,1,9);
+
+       generics.maxNumberInFloat(4.0F,5.6F,2.7F);
     }
 }
